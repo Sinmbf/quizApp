ef840072d12ffe0bbeeeb3863480cb9e639f3cbf
Sinmbf <sinmbf123@gmail.com>
2023-09-11 13:59:48 +0545

    Update main.js

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ form.addEventListener("submit",(e)=>{
     let arr=[];
     arr.push({uName:uName.value,email:email.value,gender:checkGender});
     sessionStorage.setItem("participant",JSON.stringify(arr));
-    if(sessionStorage.length>1){
+    if(sessionStorage.length>0){
         formContainer.innerHTML = `
         <div class="alert alert-success" role="alert">
             Your form has been successfully submitted! <strong> Redirecting you to the questions page...</strong>
